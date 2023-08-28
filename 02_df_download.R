e8bf3660b2bea81243461aa19b8793d1072e2582
dieghiny <diego.lpfalcon@gmail.com>
2023-08-27 18:37:20 -0600

    Archivo renombrado.

--- 02_df_download.R
+++
@@ -1,6 +0,0 @@
-library(rtweet) # Usar versión 0.7.0
-library(tidyverse)
-
-tweets_df <- search_tweets(q = "rstudio",
-                           n = 100,
-                           type = "recent")
